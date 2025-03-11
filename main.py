cf2091d6706addb55b89a5cd3a2ed54e46f87f3f
lika <user75@hotmail.fr>
2025-03-11 17:11:22 +0100

    added env,gitignore, constants and main

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
