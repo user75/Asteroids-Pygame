45029811bc96d9763febabdf693a883e02d2218d
lika <user75@hotmail.fr>
2025-03-12 16:35:32 +0100

    set up basic infinite game loop with window display & fixed display issue with wsl conf

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import pygame
-from constants import *
+import pygame #bring in pygame library 
+from constants import * # from constants.py import everything
+
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+    pygame.init() #Initialize pygame
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
 
 
 
